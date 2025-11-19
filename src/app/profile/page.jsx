7e1b56f61b917ec0cd8f47c3675f6cc565e92b9e
sanmo-yousof <sanmoyousof@gmail.com>
2025-11-19 14:08:48 +0600

    updated

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -1,5 +1,10 @@
-"use client"
+"use client";
 
+import Container from "@/components/shared/Container";
+import PageBanner from "@/components/shared/PageBanner";
+import { Button } from "@/components/ui/button";
+import { Label } from "@/components/ui/label";
+import { Building, Check, Mail, Phone } from "lucide-react";
 import { useSearchParams } from "next/navigation";
 import React from "react";
 
@@ -9,10 +14,159 @@ const Profile = () => {
   const category = searchParams.get("category");
   const id = searchParams.get("id");
 
-  return <div>
-         {category}
-         {id}
-  </div>;
+  return (
+    <>
+      <PageBanner
+        title="Profile"
+        image={
+          "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/cf136a11386527.560f6e447cc13.jpg"
+        }
+      />
+      <Container className={"py-16 grid md:grid-cols-6 gap-8"}>
+        <div className=" md:col-span-2 ">
+          <div className="p-4 rounded-md items-center  border-t-primary justify-center  border flex flex-col border-t-4">
+            <img
+              className="object-cover h-40 w-40 lg:w-60 lg:h-60  rounded-full border-4 border-white shadow-lg"
+              src={
+                "https://images.unsplash.com/photo-1672843192615-5913ef88bf17?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              }
+              alt={`profile`}
+              onError={(e) => {
+                e.target.onerror = null;
+                e.target.src = `https://placehold.co/160x160/6366f1/white?text=${profile.name.charAt(
+                  0
+                )}`;
+              }}
+            />
+            <h2 className="text-2xl mt-4 lg:text-3xl text-gray-800 font-semibold">
+              Jhon Doe
+            </h2>
+            <p className="text-sm mt-2 font-semibold text-primary">
+              House Manager
+            </p>
+          </div>
+          <div className="p-4 mt-6 rounded-md border">
+            <h2 className="subHeading mb-3">PERSONAL INFO</h2>
+            <div className="flex gap-2 items-center">
+              <Phone />
+              <div>
+                <Label className={"text-xs"}>Phone</Label>
+                <p className="text-sm mt-1 text-gray-600">+ (123) 1800-567</p>
+              </div>
+            </div>
+
+            <div className="flex gap-2 my-4 items-center">
+              <Building />
+              <div>
+                <Label className={"text-xs"}>Office</Label>
+                <p className="text-sm mt-1 text-gray-600">+ (123) 1800-567</p>
+              </div>
+            </div>
+
+            <div className="flex gap-2 my-4 items-center">
+              <Mail />
+              <div>
+                <Label className={"text-xs"}>Mail</Label>
+                <p className="text-sm mt-1 text-gray-600">example@ex.com</p>
+              </div>
+            </div>
+          </div>
+          <Button className={"w-full mt-6"} size={"lg"}>Book Now</Button>
+        </div>
+        <div className=" md:col-span-4 ">
+          <h2 className="subHeading border-b border-primary mb-4">BIO</h2>
+          <p className="text-sm">
+            Efficiently myocardinate market-driven innovation via open-source
+            alignments. Dramatically engage high-Phosfluorescently expedite
+            impactful supply chains via focused results. Holistically .
+            Compellingly supply just in time catalysts for change through..
+          </p>
+          <p className="text-sm text-gray-600 my-6">
+            Exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea
+            commodo non habent claritatem insitamconsequat duis autem facilisis
+            at vero eros vel eum iriure. Duis autem vel eum iriure dolor in
+            hendrerit in vulputate velit esse molestie consequat, vel illum
+            dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto
+            odio dignissim qui blandit praesent luptatum zzril delenit augue
+            duis dolore te feugait nulla facilisi.Exerci tation ullamcorper
+          </p>
+          <div className="my-8">
+            <h2 className="subHeading border-b border-primary mb-4">
+              BASIC INFO
+            </h2>
+            <span className="flex gap-1 flex-wrap ">
+              <Label>Location :</Label>{" "}
+              <p className="text-sm text-gray-500">USA</p>
+            </span>
+            <span className="flex gap-1 my-1 flex-wrap ">
+              <Label>Age :</Label>{" "}
+              <p className="text-sm text-gray-500">30 years old</p>
+            </span>
+            <span className="flex gap-1 flex-wrap ">
+              <Label>Language Spoken :</Label>{" "}
+              <p className="text-sm text-gray-500">Eglish, French, Arabic</p>
+            </span>
+          </div>
+
+          <div className="mb-8">
+            <h2 className="subHeading border-b border-primary mb-4">
+              EDUCATION & SKILLS
+            </h2>
+            <span className="flex flex-wrap  gap-1">
+              <Label>Degree :</Label>{" "}
+              <p className="text-sm text-gray-500">Diploma In Nursing</p>
+            </span>
+
+            <span className="flex flex-wrap  gap-1 my-1">
+              <Label>Experience in :</Label>{" "}
+              <p className="text-sm text-gray-500">
+                Basic Patient Care (Bathing, dressing, feeding, mobility),
+                Medical Assistance, Communication skills
+              </p>
+            </span>
+
+            <span className="flex gap-1 flex-wrap ">
+              <Label>Intersted in :</Label>{" "}
+              <p className="text-sm text-gray-500">
+                ElDery Care, Disability Support, Post Surgery Care
+              </p>
+            </span>
+
+             <span className="flex gap-1 mt-1 flex-wrap ">
+              <Label>Can Drive :</Label>{" "}
+              <p className="text-sm text-gray-500">
+               Yes
+              </p>
+            </span>
+          </div>
+
+          <div>
+            <h2 className="subHeading border-b border-primary mb-4">
+              Experience
+            </h2>
+            <span className="flex gap-1 flex-wrap ">
+              <Label>Years of Experience :</Label>{" "}
+              <p className="text-sm text-gray-500">
+               7
+              </p>
+            </span>
+            <span className="flex gap-1 my-1 flex-wrap ">
+              <Check className="text-primary" />
+              <Label>Nursign Council of Kenya</Label>
+            </span>
+            <span className="flex gap-1 flex-wrap ">
+              <Check className="text-primary" />
+              <Label>Hospital Based Care</Label>
+            </span>
+             <span className="flex gap-1 mt-1 flex-wrap ">
+              <Check className="text-primary" />
+              <Label>Home Based Care</Label>
+            </span>
+          </div>
+        </div>
+      </Container>
+    </>
+  );
 };
 
 export default Profile;
--- src/app/search/page.jsx
+++ src/app/search/page.jsx
@@ -4,7 +4,7 @@ import Container from "@/components/shared/Container";
 import ProfileCard from "@/components/profileCard";
 import { fakeData } from "@/utilities/data";
 import { notFound, useSearchParams } from "next/navigation";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import {
   Select,
   SelectContent,
@@ -13,47 +13,123 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import PageBanner from "@/components/shared/PageBanner";
+import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/components/ui/pagination";
 
 const Search = () => {
   const searchParams = useSearchParams();
-
   const category = searchParams.get("category");
   const startDate = searchParams.get("startDate");
   const endDate = searchParams.get("endDate");
-  if(!category || !startDate || !endDate){
-    return notFound()
+
+  const itemsPerPage = 8;
+  const [sortBy, setSortBy] = useState("");
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const sortData = useMemo(() => {
+    let data = [...fakeData];
+
+    if (sortBy === "experience") {
+      data.sort((a, b) => b.experience - a.experience);
+    } else if (sortBy === "rating") {
+      data.sort((a, b) => b.rating - a.rating);
+    }
+    return data;
+  }, [sortBy]);
+
+  // pagination
+  const totalPages = Math.ceil(sortData.length / itemsPerPage);
+
+  const currentData = useMemo(() => {
+    const start = (currentPage - 1) * itemsPerPage;
+    return sortData.slice(start, start + itemsPerPage);
+  }, [currentPage, sortData]);
+
+  const goToPage = (page) => {
+    if (page >= 1 && page <= totalPages) {
+      setCurrentPage(page);
+    }
+  };
+
+  if (!category || !startDate || !endDate) {
+    return notFound();
   }
-  
+
   return (
-    <Container className={"lg:py-16 py-12"}>
-      <div className="pb-8">
-        <div className="flex flex-col border-b sm:flex-row sm:items-center pb-2 gap-4 justify-between">
-          <h2 className="font-medium text-sm md:text-base">
-            Search by <span className="text-gray-600">{category}</span> and{" "}
-            <span className="text-gray-600">{startDate}</span> to{" "}
-            <span className="text-gray-600">{endDate}</span>
-          </h2>
+    <>
+      <PageBanner title="Our Specialist" />
+      <Container className={"lg:py-16 py-12"}>
+        <div className="pb-8">
+          <div className="flex flex-col border-b sm:flex-row sm:items-center pb-2 gap-4 justify-between">
+            <h2 className="font-medium text-sm md:text-base">
+              Search by <span className="text-gray-600">{category}</span> and{" "}
+              <span className="text-gray-600">{startDate}</span> to{" "}
+              <span className="text-gray-600">{endDate}</span>
+            </h2>
 
-          <Select>
-            <SelectTrigger className="w-[180px] border-primary">
-              <SelectValue placeholder="Sort by" />
-            </SelectTrigger>
-            <SelectContent>
-              <SelectGroup>
-                <SelectItem value="experience">Years of Experience</SelectItem>
-                <SelectItem value="rating">Rating</SelectItem>
-              </SelectGroup>
-            </SelectContent>
-          </Select>
+            <Select
+             onValueChange={(value) => {
+                setSortBy(value);
+                setCurrentPage(1); 
+              }}
+            >
+              <SelectTrigger className="w-[180px] border-primary">
+                <SelectValue placeholder="Sort by" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectGroup>
+                  <SelectItem value="experience">
+                    Years of Experience
+                  </SelectItem>
+                  <SelectItem value="rating">Rating</SelectItem>
+                </SelectGroup>
+              </SelectContent>
+            </Select>
+          </div>
         </div>
-      </div>
 
-      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
-        {fakeData.map((data, indx) => (
-          <ProfileCard key={indx} profile={data} />
-        ))}
-      </div>
-    </Container>
+        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
+          {currentData.map((data, indx) => (
+            <ProfileCard key={indx} profile={data} />
+          ))}
+        </div>
+
+        <div className="mt-16">
+          <Pagination className={"flex justify-center md:justify-end"}>
+            <PaginationContent>
+              <PaginationItem>
+                <PaginationPrevious href="#" onClick={() => goToPage(currentPage - 1)}/>
+              </PaginationItem>
+               {[...Array(totalPages)].map((_, i) => (
+                <PaginationItem key={i}>
+                  <PaginationLink
+                    href="#"
+                    isActive={currentPage === i + 1}
+                    onClick={() => goToPage(i + 1)}
+                  >
+                    {i + 1}
+                  </PaginationLink>
+                </PaginationItem>
+              ))}
+                
+              
+              
+              <PaginationItem>
+                <PaginationNext href="#" onClick={() => goToPage(currentPage + 1)}/>
+              </PaginationItem>
+            </PaginationContent>
+          </Pagination>
+        </div>
+      </Container>
+    </>
   );
 };
 
